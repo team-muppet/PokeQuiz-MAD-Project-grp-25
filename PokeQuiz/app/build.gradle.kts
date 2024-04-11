1689754b874f71032509e63dd9294f6f4db06fed
Aagaard00Laptop <Andreas_soe@hotmail.dk>
2024-04-11 13:44:36 +0200

    added nav dependency

--- PokeQuiz/app/build.gradle.kts
+++ PokeQuiz/app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
