8db8b470398404a20255a8106c5874fcaabfa50e
Gormiee <91832027+Gormiee@users.noreply.github.com>
2024-05-03 11:24:50 +0200

    kinda works
    anders fixed my stuff, shows a pp

--- PokeQuiz/app/src/main/java/com/example/pokequiz/screens/user_profile/UserProfileScreen.kt
+++ PokeQuiz/app/src/main/java/com/example/pokequiz/screens/user_profile/UserProfileScreen.kt
@@ -1,56 +1,65 @@
 package com.example.pokequiz.screens.user_profile
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import com.google.firebase.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.storage
+import kotlinx.coroutines.flow.MutableStateFlow
 
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun UserProfileScreen(viewModel: UserProfileViewModel = hiltViewModel()) {
-    val storage: FirebaseStorage = FirebaseStorage.getInstance()
-    val storageRef: StorageReference = storage.getReferenceFromUrl("gs://pokequiz-1d3fc.appspot.com/public/Surprised_Pikachu_HD_FB.jpg")
-
-    val painter3 = rememberAsyncImagePainter(storageRef)
-
+    val downloadUrl = remember { mutableStateOf("") }
+    val imageUrl = remember { mutableStateOf("") }
     val profile = viewModel.userProfile.collectAsState(emptyList())
     val username = profile.value.getOrNull(0)?.username ?: "USERNAME"
-    val imageUrl = "gs://pokequiz-1d3fc.appspot.com/public/Surprised_Pikachu_HD_FB.jpg"
-    val storageReference = Firebase.storage.getReferenceFromUrl(imageUrl)
+    println("username URL: $username")
+    val userid = profile.value.getOrNull(0)?.profileImg ?: "userid"
+    println("userid URL: $userid")
+    imageUrl.value = profile.value.getOrNull(0)?.profileImg ?: "No"
+    println("imageUrl URL: $imageUrl")
+
+    val storageReference = if (imageUrl.value != "No") {
+        Firebase.storage.getReferenceFromUrl(imageUrl.value)
+    } else {
+        Firebase.storage.getReferenceFromUrl("gs://pokequiz-1d3fc.appspot.com/public/Surprised_Pikachu_HD_FB.jpg")
+    }
     // Generate the download URL for the file
-    var downloadUrl = ""
     storageReference.downloadUrl.addOnSuccessListener { uri ->
-        downloadUrl = uri.toString()
+        downloadUrl.value = uri.toString()
         // Use the download URL to access the file
         println("Download URL: $downloadUrl")
     }.addOnFailureListener { exception ->
         // Handle any errors that occur while generating the download URL
         println("Error generating download URL: ${exception.message}")
     }
-    val painter = rememberAsyncImagePainter(downloadUrl)
-    var fuckme = rememberAsyncImagePainter(model = downloadUrl)
-    val painter2 = rememberAsyncImagePainter(model = "https://firebasestorage.googleapis.com/v0/b/pokequiz-1d3fc.appspot.com/o/public%2FSurprised_Pikachu_HD_FB.jpg?alt=media&token=919a8fe3-82ed-4375-8289-fffd0256c6b4")
     Column(
     ) {
 
-        Image(
-            painter = painter2,
-            contentDescription = null,
+        AsyncImage(
+            model = downloadUrl.value,
+            contentDescription = "Mpuuetman",
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp) // Adjust the height as needed
+                .height(200.dp) // Adjust the height as needed)
         )
-                    Spacer(modifier = Modifier.height(16.dp))
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         OutlinedTextField(
             value = username,
